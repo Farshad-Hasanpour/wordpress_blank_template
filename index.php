1213e12a3899b33637009ae62c98ba87273ce6d3
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2022-07-12 22:46:53 +0430

    add support for older php versions

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-http_response_code(403);
+// http_response_code(403);
+$protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+header($protocol . ' 403 Forbidden');
+
 die();
